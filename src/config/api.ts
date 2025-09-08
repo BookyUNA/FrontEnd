34537fda1b88359581eaced97cd00c67cf45bcd6
albertone448 <107166661+albertone448@users.noreply.github.com>
2025-09-08 10:17:46 -0600

    link local comentado

--- src/config/api.ts
+++ src/config/api.ts
@@ -4,6 +4,10 @@
 
 // Base URL para todas las peticiones API
 export const API_CONFIG = {
+  //Localhost para emulador Android:
+  //BASE_URL: 'http://10.0.2.2:61288/api',
+
+  //Servidor:
   BASE_URL: 'https://backendbooky-apis-b8b9fzgxfadrdch7.canadacentral-01.azurewebsites.net/api',
   ENDPOINTS: {
     LOGIN: '/Login',

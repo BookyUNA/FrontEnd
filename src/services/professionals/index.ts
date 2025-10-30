b433fe607445f468ea2b865fd87d628ef514eb7d
Alberto Nunez Trejos <albertone448@gmail.com>
2025-10-30 16:40:42 -0600

    FEAT: Implementar servicio de pagos con Onvo y asignación de planes

---
+++ src/services/professionals/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Exportaciones del módulo de Profesionales
+ * services/professionals/index.ts
+ */
+
+export { 
+  paymentService,
+  type PaymentRequest,
+  type PaymentResponse,
+  type AssignPlanRequest,
+  type AssignPlanResponse,
+  type PaymentFormData,
+  type SelectedPlan,
+  type PaymentResult,
+  type PaymentStatus,
+  type PlanType,
+  PLAN_NUMBERS,
+} from './paymentService';
